2f660a9b26d81da3df629c185388347faea2baef
Diego Neri <diego@diegoneri.com>
2021-06-25 18:55:08 -0300

    Cria projeto console .NET Core

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjetoFinal202101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
