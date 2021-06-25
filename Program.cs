d847a6b69b428aad5eac29d82708d5c4be459e73
Diego Neri <diego@diegoneri.com>
2021-06-25 19:40:01 -0300

    Cria o menu de opcoes

--- Program.cs
+++ Program.cs
@@ -6,7 +6,31 @@ namespace ProjetoFinal202101
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string opcao;
+
+            do
+            {
+                exibeMenu();
+                opcao = Console.ReadLine();
+            }
+            while (opcao != "X");
+
+        }
+
+        static void exibeMenu()
+        {
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("|           Menu          |");
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("| S - Soma (+)            |");
+            Console.WriteLine("| B - Subtração (-)       |");
+            Console.WriteLine("| M - Multiplicação (x)   |");
+            Console.WriteLine("| D - Divisão (/)         |");
+            Console.WriteLine("| E - Exponenciação (¹²³) |");
+            Console.WriteLine("| R - Radiciação (V)      |");
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("| A - Sobre o programa    |");
+            Console.WriteLine("---------------------------");
         }
     }
 }
